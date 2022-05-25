d70763d75a525a3ead99cf7a8cdaf33fd418a971
M4teusPrado <mateus.silvaprado@gmail.com>
2022-05-24 22:37:27 -0300

    Criação da estrutura de validações de erros

---
+++ src/main/java/com/pa/sobrouemcasa/erros/ErrorObject.java
@@ -0,0 +1,14 @@
+package com.pa.sobrouemcasa.erros;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class ErrorObject {
+
+    private final String message;
+    private final String field;
+    private final Object parameter;
+
+}
